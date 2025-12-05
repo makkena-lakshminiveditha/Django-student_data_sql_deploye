d3ee3a9621f28fa7690032872469c78793dfabc6
makkena-lakshminiveditha <makkenalakshminiveditha@gmail.com>
2025-12-05 10:05:59 +0530

    msg

--- student_data/settings.py
+++ student_data/settings.py
@@ -73,14 +73,39 @@ WSGI_APPLICATION = 'student_data.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+import os
+if os.environ.get("RENDER"):
+# Production (Render + Railway MySQL)
+    DATABASES = {
+    "default": {
+    "ENGINE": "django.db.backends.mysq1",
+    "NAME": os.environ.get("MYSQLDATABASE"),
+    "USER": os.environ.get("MYSQLUSER"),
+    "PASSWORD": os.environ.get("MYSQLPASSWORD"),
+    "HOST": os.environ.get("MYSQLHOST"),
+    "PORT": os.environ.get("MYSQLPORT", "3306"),
+
     }
+
 }
-
-
+else:
+# Local development
+    DATABASES = {
+        "default": {
+        "ENGINE": "django.db.backends.mysq1",
+        "NAME": "bank_project",
+        "USER": "root",
+        "PASSWORD": "Naganivi@123",
+        "HOST": "localhost",
+        "PORT": "3306",
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
